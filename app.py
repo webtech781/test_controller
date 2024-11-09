e10be998f70ecf4699b54a41f2c89e2ce89171b8
vamshi krishna <139684496+webtech781@users.noreply.github.com>
2024-11-09 12:28:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pyAesCrypt
 import xml.etree.ElementTree as ET
 from secure_delete import secure_delete
 
-TOKEN = '6497003143:AAG3mGljFOWWGV1AZs6cOn8TFw6yqztCkyY'  
+TOKEN = '...'  
 
 bot = telebot.TeleBot(TOKEN)
 cd = os.path.expanduser("~")
